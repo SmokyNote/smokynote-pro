d5af83d3e3096847262f372b63177433fc5460e8
ratsam <m4spam@gmail.com>
2013-12-18 13:53:15 +0400

    Get ready for instrumentation testing.

---
+++ app/instrumentation/src/main/java/com/smokynote/instrumentation/NotesListActivityTest.java
@@ -0,0 +1,21 @@
+package com.smokynote.instrumentation;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.smokynote.*;
+import com.smokynote.R;
+
+/**
+ * @author Maksim Zakharov
+ * @since 1.0
+ */
+public class NotesListActivityTest extends ActivityInstrumentationTestCase2<NotesListActivity> {
+
+    public NotesListActivityTest() {
+        // Android 2.1 requires pkg to be presented.
+        super("com.smokynote", NotesListActivity.class);
+    }
+
+    public void testRecordButtonPresented() {
+        assertNotNull("Expected 'Record note' button to be presented", getActivity().findViewById(R.id.add_record));
+    }
+}
