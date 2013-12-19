4522c34d6af73e984c5e8ae5b35258279c10a75a
ratsam <m4spam@gmail.com>
2013-12-19 11:02:35 +0400

    Integrate Travis

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/ratsam/smokynote-pro.png?branch=master)](https://travis-ci.org/ratsam/smokynote-pro)
---
+++ ci/install_android_sdk.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/expect -f
+
+# Script installs Android SDK components
+# For a full list, run `android list sdk -a --extended`
+set timeout -1;
+spawn android update sdk --filter tools,platform-tools,build-tools-19.0.0,$env(ANDROID_SDKS),extra-android-support --no-ui --force
+expect { 
+    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+    eof
+}
