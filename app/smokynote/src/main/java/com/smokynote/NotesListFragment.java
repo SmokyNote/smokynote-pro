f08d252d1c0137211ef05bfc8ba858f56a4d0596
ratsam <m4spam@gmail.com>
2013-12-19 19:19:15 +0400

    Set list adapter for notes list fragment.

--- app/smokynote/src/main/java/com/smokynote/NotesListFragment.java
+++ app/smokynote/src/main/java/com/smokynote/NotesListFragment.java
@@ -1,5 +1,9 @@
 package com.smokynote;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import com.actionbarsherlock.app.SherlockListFragment;
 
 /**
@@ -7,4 +11,18 @@ import com.actionbarsherlock.app.SherlockListFragment;
  * @since 1.0
  */
 public class NotesListFragment extends SherlockListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setListAdapter(new NotesListAdapter(getActivity()));
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
