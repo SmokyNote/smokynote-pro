fb9a1c9f6abf8c7ec34fd5476bb4b0d12a5d1792
ratsam <m4spam@gmail.com>
2013-12-20 17:07:57 +0400

    Test Notes list rendering.

--- app/instrumentation/src/main/java/com/smokynote/instrumentation/NotesListActivityIntegrationTest.java
+++ app/instrumentation/src/main/java/com/smokynote/instrumentation/NotesListActivityIntegrationTest.java
@@ -1,8 +1,16 @@
 package com.smokynote.instrumentation;
 
 import android.test.ActivityInstrumentationTestCase2;
-import com.smokynote.*;
+import android.widget.ListView;
+import com.smokynote.Application;
+import com.smokynote.NotesListActivity;
 import com.smokynote.R;
+import com.smokynote.note.NotesRepository;
+import com.smokynote.orm.Note;
+import org.joda.time.DateTime;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 /**
  * Integration test for {@link NotesListActivity}.
@@ -12,12 +20,47 @@ import com.smokynote.R;
  */
 public class NotesListActivityIntegrationTest extends ActivityInstrumentationTestCase2<NotesListActivity> {
 
+    Application application;
+
     public NotesListActivityIntegrationTest() {
         // Android 2.1 requires pkg to be presented.
         super("com.smokynote", NotesListActivity.class);
     }
 
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        application = ((Application) getInstrumentation().getTargetContext().getApplicationContext());
+        application.onCreate();
+    }
+
     public void testRecordButtonPresented() {
         assertNotNull("Expected 'Record note' button to be presented", getActivity().findViewById(R.id.add_note));
+    }
+
+    public void testNotesPresented() {
+        updateNotesRepository();
+
+        getInstrumentation().waitForIdleSync();
+
+        assertThat("Expected exactly one Note to be listed", listView().getChildCount(), equalTo(1));
+    }
+
+    private void updateNotesRepository() {
+        final NotesRepository notesRepository = application.getDependency(NotesRepository.class);
+        notesRepository.clear();
+        notesRepository.add(createTestNote());
+    }
+
+    private Note createTestNote() {
+        final Note note = new Note();
+        note.setId(1);
+        note.setSchedule(DateTime.now());
+        return note;
+    }
+
+    private ListView listView() {
+        return (ListView) getActivity().findViewById(android.R.id.list);
     }
 }
