4003485461f24f36aaa22a2895e79f93d3bc298f
Maksim Zakharov <python.candidate@gmail.com>
2013-12-24 01:57:18 +0400

    Launch recording activity by button click.

--- app/smokynote/src/main/java/com/smokynote/NotesListActivity.java
+++ app/smokynote/src/main/java/com/smokynote/NotesListActivity.java
@@ -1,8 +1,12 @@
 package com.smokynote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.smokynote.record.RecordActivity;
 
 /**
  * @author Maksim Zakharov
@@ -22,10 +26,26 @@ public class NotesListActivity extends SherlockFragmentActivity {
         super.onPostCreate(savedInstanceState);
 
         initActionBar();
+        initRecordButton();
     }
 
     private void initActionBar() {
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_SHOW_HOME);
+    }
+
+    private void initRecordButton() {
+        final Button recordButton = (Button) findViewById(R.id.add_note);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchRecordActivity();
+            }
+        });
+    }
+
+    private void launchRecordActivity() {
+        final Intent intent = new Intent(this, RecordActivity.class);
+        startActivity(intent);
     }
 }
