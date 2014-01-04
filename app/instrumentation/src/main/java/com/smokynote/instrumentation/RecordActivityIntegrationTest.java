8217ad2cacd5708cd65c0a18c3f9c92f20a30ef9
Maksim Zakharov <python.candidate@gmail.com>
2014-01-05 00:33:05 +0400

    Instrumentation test for RecordActivity.

---
+++ app/instrumentation/src/main/java/com/smokynote/instrumentation/RecordActivityIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.smokynote.instrumentation;
+
+import android.content.pm.ActivityInfo;
+import android.support.v4.app.Fragment;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.smokynote.Application;
+import com.smokynote.R;
+import com.smokynote.record.RecordActivity;
+
+/**
+ * @author Maksim Zakharov
+ * @since 1.0
+ */
+public class RecordActivityIntegrationTest extends ActivityInstrumentationTestCase2<RecordActivity> {
+
+    Application application;
+
+    public RecordActivityIntegrationTest() {
+        super("com.smokynote", RecordActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        application = ((Application) getInstrumentation().getTargetContext().getApplicationContext());
+        application.onCreate();
+    }
+
+    public void testStorageUnmounted() {
+        // TODO
+    }
+
+    @MediumTest
+    public void testFragmentRetain() {
+        final SherlockFragmentActivity activity = getActivity();
+        final Fragment portraitFragment = activity.getSupportFragmentManager().findFragmentById(R.id.record_fragment);
+
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getInstrumentation().waitForIdleSync();
+
+        final Fragment landscapeFragment = getActivity().getSupportFragmentManager().findFragmentById(R.id.record_fragment);
+        assertTrue("Recording fragment must be retained", portraitFragment == landscapeFragment);
+    }
+}
