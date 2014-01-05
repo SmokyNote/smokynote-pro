b0b4b14b810cf7c48ee80f614486cb90598fbd1c
Maksim Zakharov <python.candidate@gmail.com>
2014-01-05 18:52:39 +0400

    Move RecordActivity#shouldUseActionsInsteadOfButtonBar to DialogActivity.

--- app/smokynote/src/main/java/com/smokynote/activity/DialogActivity.java
+++ app/smokynote/src/main/java/com/smokynote/activity/DialogActivity.java
@@ -1,6 +1,7 @@
 package com.smokynote.activity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.ViewGroup;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.smokynote.R;
@@ -20,5 +21,23 @@ public abstract class DialogActivity extends SherlockFragmentActivity {
             // This sets the window size, while working around the IllegalStateException thrown by ActionBarView
             getWindow().setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT);
         }
+    }
+
+    /**
+     * On small screens we prefer to show action buttons as part of actionbar
+     * rather then to acquire extra space for additional button bar.
+     *
+     * @return true if (bottom) button bar should be replaced with actionbar action buttons
+     */
+    protected boolean shouldUseActionsInsteadOfButtonBar() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        return convertPxToDip(metrics.heightPixels) < 350;
+    }
+
+    private int convertPxToDip(int pixel){
+        float scale = getResources().getDisplayMetrics().density;
+        return (int) ((pixel / scale) + 0.5f);
     }
 }
--- app/smokynote/src/main/java/com/smokynote/record/RecordActivity.java
+++ app/smokynote/src/main/java/com/smokynote/record/RecordActivity.java
@@ -138,24 +138,6 @@ public class RecordActivity extends DialogActivity implements RecordListener {
         finish();
     }
 
-    /**
-     * On small screens we prefer to show action buttons as part of actionbar
-     * rather then to acquire extra space for additional button bar.
-     *
-     * @return true if (bottom) button bar should be replaced with actionbar action buttons
-     */
-    private boolean shouldUseActionsInsteadOfButtonBar() {
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-        return convertPxToDip(metrics.heightPixels) < 350;
-    }
-
-    private int convertPxToDip(int pixel){
-        float scale = getResources().getDisplayMetrics().density;
-        return (int) ((pixel / scale) + 0.5f);
-    }
-
     @Override
     public void onStorageUnavailable() {
         LOG.warn("Storage unavailable");
