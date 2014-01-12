6da2993976eb234569c8a15a0c177b24909f69a0
Maksim Zakharov <python.candidate@gmail.com>
2014-01-12 20:42:18 +0400

    Add TimePicker activity.

---
+++ app/smokynote/src/main/java/com/smokynote/timer/TimePickerActivity.java
@@ -0,0 +1,43 @@
+package com.smokynote.timer;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import com.actionbarsherlock.app.ActionBar;
+import com.smokynote.R;
+import com.smokynote.activity.DialogActivity;
+
+/**
+ * @author Maksim Zakharov
+ * @since 1.0
+ */
+public class TimePickerActivity extends DialogActivity {
+
+    private TimePickerFragment timePickerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.dialog_activity);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        timePickerFragment = (TimePickerFragment) fragmentManager.findFragmentById(R.id.dialog_fragment);
+        if (timePickerFragment == null) {
+            timePickerFragment = new TimePickerFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.dialog_fragment, timePickerFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void initActionBar() {
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
+    }
+
+    @Override
+    protected void bindButtonBar() {
+
+    }
+}
---
+++ app/smokynote/src/main/java/com/smokynote/timer/TimePickerFragment.java
@@ -0,0 +1,55 @@
+package com.smokynote.timer;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import com.actionbarsherlock.app.SherlockFragment;
+import com.smokynote.R;
+import org.joda.time.DateTime;
+
+/**
+ * @author Maksim Zakharov
+ * @since 1.0
+ */
+public class TimePickerFragment extends SherlockFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.date_time_pick, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        final DateTime targetTime = getTargetTime();
+        final DatePicker datePicker = (DatePicker) view.findViewById(R.id.date_picker);
+        final TimePicker timePicker = (TimePicker) view.findViewById(R.id.time_picker);
+
+        datePicker.updateDate(targetTime.getYear(), targetTime.getMonthOfYear() - 1, targetTime.getDayOfMonth());
+        if (Build.VERSION.SDK_INT >= 11) {
+            datePicker.setMinDate(getMinDateInMilliseconds());
+        }
+        timePicker.setIs24HourView(DateFormat.is24HourFormat(getActivity()));
+        timePicker.setCurrentHour(targetTime.getHourOfDay());
+        timePicker.setCurrentMinute(targetTime.getMinuteOfHour());
+    }
+
+    private DateTime getTargetTime() {
+        return DateTime.now().plusMinutes(15);
+    }
+
+    private long getMinDateInMilliseconds() {
+        return DateTime.now().minusSeconds(1).getMillis();
+    }
+}
