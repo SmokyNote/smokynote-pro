b0025ddabee8605d860b3258b7188d58da7e5f3e
ratsam <m4spam@gmail.com>
2014-08-29 20:56:39 +0400

    Target new Android version.

--- ci/install_android_sdk.sh
+++ ci/install_android_sdk.sh
@@ -3,7 +3,7 @@
 # Script installs Android SDK components
 # For a full list, run `android list sdk -a --extended`
 set timeout -1;
-spawn android update sdk --filter tools,platform-tools,build-tools-19.0.1,$env(ANDROID_SDKS),extra-android-support --no-ui --force
+spawn android update sdk --filter tools,platform-tools,build-tools-20.0.0,$env(ANDROID_SDKS),extra-android-support --no-ui -all --force
 expect { 
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
