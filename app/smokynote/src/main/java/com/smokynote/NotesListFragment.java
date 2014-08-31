c71ac8d4062d0d76262182d23680a3fbd831e07a
ratsam <m4spam@gmail.com>
2014-08-31 17:06:39 +0400

    Refactoring: extract inner class NotesListFragment#NoteMenuCallback.

--- app/smokynote/src/main/java/com/smokynote/NotesListFragment.java
+++ app/smokynote/src/main/java/com/smokynote/NotesListFragment.java
@@ -1,5 +1,6 @@
 package com.smokynote;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -84,34 +85,49 @@ public class NotesListFragment extends SherlockListFragment implements NotesList
     }
 
     @Override
-    public void onMenuRequested(final Note note, View anchor) {
-        final MenuBuilder menuBuilder = new MenuBuilder(anchor.getContext());
-        new MenuInflater(anchor.getContext()).inflate(R.menu.note_context_menu, menuBuilder);
-        menuBuilder.setCallback(new MenuBuilder.Callback() {
-            @Override
-            public boolean onMenuItemSelected(MenuBuilder menu, MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.note_action_play:
-                        LOG.info("Selected Play action for Note {}", note);
-                        return true;
-                    case R.id.note_action_schedule:
-                        LOG.info("Selected Schedule action for Note {}", note);
-                        return true;
-                    case R.id.note_action_delete:
-                        LOG.info("Selected Delete action for Note {}", note);
-                        return true;
-                    default:
-                        LOG.warn("Unhandled menu item with id = {}", item.getItemId());
-                        return false;
-                }
-            }
-
-            @Override
-            public void onMenuModeChange(MenuBuilder menu) {
-                // pass
-            }
-        });
-        menuPopupHelper = new MenuPopupHelper(anchor.getContext(), menuBuilder, anchor);
+    public void onMenuRequested(Note note, View anchor) {
+        final Context context = anchor.getContext();
+        final MenuBuilder menuBuilder = createMenuBuilder(note, context);
+        menuPopupHelper = new MenuPopupHelper(context, menuBuilder, anchor);
         menuPopupHelper.show();
+    }
+
+    private MenuBuilder createMenuBuilder(Note note, Context context) {
+        final MenuBuilder menuBuilder = new MenuBuilder(context);
+        new MenuInflater(context).inflate(R.menu.note_context_menu, menuBuilder);
+        menuBuilder.setCallback(new NoteMenuCallback(note));
+        return menuBuilder;
+    }
+
+    private class NoteMenuCallback implements MenuBuilder.Callback {
+
+        private final Note targetNote;
+
+        private NoteMenuCallback(Note targetNote) {
+            this.targetNote = targetNote;
+        }
+
+        @Override
+        public boolean onMenuItemSelected(MenuBuilder menu, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.note_action_play:
+                    LOG.info("Selected Play action for Note {}", targetNote);
+                    return true;
+                case R.id.note_action_schedule:
+                    LOG.info("Selected Schedule action for Note {}", targetNote);
+                    return true;
+                case R.id.note_action_delete:
+                    LOG.info("Selected Delete action for Note {}", targetNote);
+                    return true;
+                default:
+                    LOG.warn("Unhandled menu item with id = {}", item.getItemId());
+                    return false;
+            }
+        }
+
+        @Override
+        public void onMenuModeChange(MenuBuilder menu) {
+            // pass
+        }
     }
 }
