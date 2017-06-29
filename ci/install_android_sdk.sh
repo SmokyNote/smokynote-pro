5b0f1c45e1b359e5504ae13ee17835075689b526
Maksim Zakharov <m4spam@gmail.com>
2017-06-29 13:39:05 +0300

    Update Android SDK tools.

--- ci/install_android_sdk.sh
+++ ci/install_android_sdk.sh
@@ -3,8 +3,8 @@
 # Script installs Android SDK components
 # For a full list, run `android list sdk -a --extended`
 set timeout -1;
-spawn android update sdk --filter tools,platform-tools,build-tools-20.0.0,$env(ANDROID_SDKS),extra-android-support --no-ui --all --force
-expect { 
-    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+spawn sdkmanager "tools" "platform-tools" "build-tools;25.0.3" "extras;android;m2repository" $env(ANDROID_PLATFORM) $env(ANDROID_IMAGE)
+expect {
+    "Accept?" { exp_send "y\r" ; exp_continue }
     eof
 }
