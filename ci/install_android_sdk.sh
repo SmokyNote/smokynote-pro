093b38f0b78297c79ff33729c6ed24aab928c9a4
Maksim Zakharov <m4spam@gmail.com>
2017-06-30 17:58:26 +0300

    Install Emulator using sdkmanager

--- ci/install_android_sdk.sh
+++ ci/install_android_sdk.sh
@@ -3,7 +3,7 @@
 # Script installs Android SDK components
 # For a full list, run `android list sdk -a --extended`
 set timeout -1;
-spawn sdkmanager "tools" "platform-tools" "build-tools;25.0.3" "extras;android;m2repository" $env(ANDROID_PLATFORM) $env(ANDROID_IMAGE)
+spawn sdkmanager "tools" "platform-tools" "build-tools;25.0.3" "extras;android;m2repository" "emulator" $env(ANDROID_PLATFORM) $env(ANDROID_IMAGE)
 expect {
     "Accept?" { exp_send "y\r" ; exp_continue }
     eof
